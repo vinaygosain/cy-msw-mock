786763f8be10b704d17b7224125bb46f6170f34e
vinay <vinay.gosain@olx.com>
2021-11-08 16:14:40 +0530

    Make singleton

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 /* global window */
 
-const server = require('./lib/server');
-const browser = require('./lib/browser');
+const ServerMock = require('./lib/server');
+const BrowserMock = require('./lib/browser');
 
-let mockServer = browser;
-if (typeof window === 'undefined') {
-    mockServer = server;
+
+function mockServer(config) {
+    let browserMockInstance = null;
+    let serverMockInstance = null;
+    return {
+        init: function () {
+            if (typeof window === 'undefined') {
+                if (!serverMockInstance) {
+                    serverMockInstance = new ServerMock(config);
+                }
+                serverMockInstance.init();
+
+                return serverMockInstance;
+            }
+
+            if (!browserMockInstance) {
+                browserMockInstance = new BrowserMock(config);
+            }
+            browserMockInstance.init();
+            return browserMockInstance;
+        },
+        override: function (overrideConfig) {
+            if (browserMockInstance) {
+                browserMockInstance.init(overrideConfig);
+            }
+
+            if (serverMockInstance) {
+                serverMockInstance.init(overrideConfig);
+            }
+        }
+    }
 }
 
 module.exports = mockServer;
